093bc9bb2ea47589ec40de5af0fdcfc813664e03
Dev-JamesR <admin@ether1.org>
2019-05-29 04:27:25 +0000

    Chain ID Update - Prep For 2nd Testnet

--- params/config.go
+++ params/config.go
@@ -78,7 +78,7 @@ var TrustedCheckpoints = map[common.Hash]*TrustedCheckpoint{
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:             big.NewInt(1313118),
+		ChainID:             big.NewInt(1313119),
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        nil,
 		DAOForkSupport:      true,
